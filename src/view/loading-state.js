d752a584d7af19395af89a4a72fe829debaa0076
Valeria Khristinina <valeria.khristinina@gmail.com>
2021-07-28 23:15:43 +0200

    Created more modules

---
+++ src/view/loading-state.js
@@ -0,0 +1,3 @@
+export const createLoadingState = () => (
+  '<p class="trip-events__msg">Loading...</p>'
+);
---
+++ src/view/trips-list.js
@@ -0,0 +1,7 @@
+export const createTripsList = () => (
+  `<ul class="trip-events__list">
+    <li class="trip-events__item">
+
+    </li>
+  </ul>`
+);
