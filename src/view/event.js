b50d1724d524ed2fb075acc5d3427659346ceae6
Valeria Khristinina <valeria.khristinina@gmail.com>
2021-08-04 16:41:11 +0200

    edited event template

--- src/view/event.js
+++ src/view/event.js
@@ -1,30 +1,48 @@
-export const createEvent = () => (
-  `<li class="trip-events__item">
+import dayjs from 'dayjs';
+const list = document.querySelector('.event__selected-offers');
+console.log(list);
+
+const createListOffersTemplate = (offers) => {
+  offers.forEach(offer => {
+  return `<li class="event__offer">
+      <span class="event__offer-title">${offer.type}</span>
+      &plus;&euro;&nbsp;
+      <span class="event__offer-price">${offer.price}</span>
+    </li>`
+  }
+};
+
+export const createEvent = (task) => {
+
+  const {datePoint, type, destination, time, price, offers} = task;
+
+  const date = datePoint !== null
+    ? dayjs(datePoint).format('MMM D')
+    : '';
+
+  return `<li class="trip-events__item">
     <div class="event">
-      <time class="event__date" datetime="2019-03-18">MAR 18</time>
+      <time class="event__date" datetime="2019-03-18">${date}</time>
       <div class="event__type">
-        <img class="event__type-icon" width="42" height="42" src="img/icons/taxi.png" alt="Event type icon">
+        <img class="event__type-icon" width="42" height="42" src="img/icons/${type}.png" alt="Event type icon">
       </div>
-      <h3 class="event__title">Taxi Amsterdam</h3>
+      <h3 class="event__title">${type} ${destination.name}</h3>
       <div class="event__schedule">
         <p class="event__time">
-          <time class="event__start-time" datetime="2019-03-18T10:30">10:30</time>
+          <time class="event__start-time" datetime="2019-03-18T10:30">${time.from}</time>
           &mdash;
-          <time class="event__end-time" datetime="2019-03-18T11:00">11:00</time>
+          <time class="event__end-time" datetime="2019-03-18T11:00">${time.to}</time>
         </p>
-        <p class="event__duration">30M</p>
+        <p class="event__duration">${time.duration}M</p>
       </div>
       <p class="event__price">
-        &euro;&nbsp;<span class="event__price-value">20</span>
+        &euro;&nbsp;<span class="event__price-value">${price}</span>
       </p>
       <h4 class="visually-hidden">Offers:</h4>
       <ul class="event__selected-offers">
-        <li class="event__offer">
-          <span class="event__offer-title">Order Uber</span>
-          &plus;&euro;&nbsp;
-          <span class="event__offer-price">20</span>
-        </li>
+        ${createListOffersTemplate(offers)}
       </ul>
+
       <button class="event__favorite-btn event__favorite-btn--active" type="button">
         <span class="visually-hidden">Add to favorite</span>
         <svg class="event__favorite-icon" width="28" height="28" viewBox="0 0 28 28">
@@ -35,5 +53,5 @@ export const createEvent = () => (
         <span class="visually-hidden">Open event</span>
       </button>
     </div>
-  </li>`
-);
+  </li>`;
+};
