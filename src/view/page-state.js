c39328db46ec7d1465ae359309a911bcfa37efdf
Valeria Khristinina <valeria.khristinina@gmail.com>
2021-08-11 16:20:31 +0200

    created new class PageState

--- src/view/page-state.js
+++ src/view/page-state.js
@@ -1,4 +1,6 @@
-export const createPageState = (state) => {
+import {createElement} from '@/utils.js';
+
+const createPageState = (state) => {
   if (state === 'loading') {
     return '<p class="trip-events__msg">Loading...</p>';
   }
@@ -7,3 +9,25 @@ export const createPageState = (state) => {
   }
 
 };
+
+export default class PageState {
+  constructor(state) {
+    this._state = state;
+    this._element = null;
+  }
+
+  getTemplate() {
+    return createPageState(this._state);
+  }
+
+  getElement() {
+    if (!this._element) {
+      this._element = createElement(this.getTemplate());
+    }
+    return this._element;
+  }
+
+  removeElement() {
+    this._element = null;
+  }
+}
